22c45e8e2607d8a6ce6d3074b2a1cfe73c2fc2e0
yunhes <yunhes@uci.edu>
2019-07-06 23:15:42 +0800

    initialization

---
+++ iOS/UC_carpool/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UC_carpool
+//
+//  Created by 邵云鹤 on 7/6/19.
+//  Copyright © 2019 UCCp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func SubmitFinal(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ iOS/UC_carpoolTests/UC_carpoolTests.swift
@@ -0,0 +1,34 @@
+//
+//  UC_carpoolTests.swift
+//  UC_carpoolTests
+//
+//  Created by 邵云鹤 on 7/6/19.
+//  Copyright © 2019 UCCp. All rights reserved.
+//
+
+import XCTest
+@testable import UC_carpool
+
+class UC_carpoolTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
