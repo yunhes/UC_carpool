3db5c99a3c200a0502093cb7b009103b660d872e
邵云鹤 <shao@ShaoYHdeMac-mini.local>
2019-07-07 23:00:30 +0800

    update textfield

--- iOS/UC_carpool/ViewController.swift
+++ iOS/UC_carpool/ViewController.swift
@@ -9,14 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var start = ""
+    var destination = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func SubmitFinal(_ sender: UIButton) {
+        //TODO: calculate the roate/send the result to sever and let server calculate
     }
     
+    @IBAction func finishStart(_ sender: UITextField) {
+        start = startReturn(sender)
+    }
+    
+    
+    @IBAction func finishdest(_ sender: UITextField) {
+        destination = destReturn(sender)
+    }
+    
+    func startReturn(_ startLoc: UITextField) -> String {
+        var Start = ""
+        startLoc.resignFirstResponder()
+        Start = startLoc.text ?? ""
+        return Start
+    }
+    
+    func destReturn(_ destiLoc: UITextField) -> String {
+        var Start = ""
+        destiLoc.resignFirstResponder()
+        Start = destiLoc.text ?? ""
+        return Start
+    }
 }
+
 
